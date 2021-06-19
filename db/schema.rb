46fdc38185ead4f37f44a4f61d446fe305047b9f
jie7206 <1524556052@qq.com>
2021-06-19 19:17:31 +0800

    由远端更新migrate文件名

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_121303) do
+ActiveRecord::Schema.define(version: 2021_06_14_135333) do
 
   create_table "currencies", force: :cascade do |t|
     t.string "name"
