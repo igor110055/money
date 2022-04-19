35d589ce1437bb583137ae8ea5856105fb181453
jie7206 <1524556052@qq.com>
2022-04-19 20:50:59 +0800

    试算表显示储备余额与生活月数栏位

---
+++ test/invest_test.rb
@@ -0,0 +1,15 @@
+count = 0
+sum = 0
+from = 6000
+to = 4000
+months = 12*3
+sp = ((to-from)/months).to_i
+invest_array = (from..to).step(sp).to_a
+invest_array.each do |n|
+  sum += n
+  puts "#{count}:#{n}:#{sum}"
+  count += 1
+end
+# puts "Invest Max:#{(300000/4.6).to_i} CNY"
+puts "Invest Max:#{2300*36} CNY"
+puts "$trial_month_invest = (#{from}..#{to}).step(#{sp}).to_a"
