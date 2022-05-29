fd10886b54b9b0a50570a9ba3ad81985ff209b50
jie7206 <1524556052@qq.com>
2022-05-29 18:46:05 +0800

    Cleared git cache and .py files

--- py/look_order.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from HuobiServices import *
-import sys
-import json
-
-
-if __name__ == '__main__':
-    order_id = sys.argv[1].split('=')[1]
-    json.dump(order_info(order_id), sys.stdout)
--- py/timestamp.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import sys
-from datetime import datetime
-import time
-
-if __name__ == '__main__':
-    print(int(time.time()), sys.stdout)
-
